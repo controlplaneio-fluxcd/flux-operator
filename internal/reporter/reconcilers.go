8cebd155ae35dcc11bd781df226ca6e32134c310
Stefan Prodan <stefan.prodan@gmail.com>
2025-06-22 12:09:05 +0300

    Add ResourceSet stats to FluxReport
    Signed-off-by: Stefan Prodan <stefan.prodan@gmail.com>

--- internal/reporter/reconcilers.go
+++ internal/reporter/reconcilers.go
@@ -10,10 +10,12 @@ import (
 
 	"github.com/fluxcd/cli-utils/pkg/kstatus/status"
 	"github.com/fluxcd/pkg/apis/meta"
+	ssautil "github.com/fluxcd/pkg/ssa/utils"
 	"golang.org/x/exp/slices"
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+	"k8s.io/apimachinery/pkg/runtime/schema"
 	kerrors "k8s.io/apimachinery/pkg/util/errors"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 
@@ -75,9 +77,69 @@ func (r *FluxStatusReporter) getReconcilersStatus(ctx context.Context, crds []me
 		}
 	}
 
+	opStats, err := r.getOperatorReconcilersStatus(ctx)
+	if err != nil {
+		multiErr = kerrors.NewAggregate([]error{multiErr, err})
+	} else {
+		resStats = append(resStats, opStats...)
+	}
+
 	slices.SortStableFunc(resStats, func(i, j fluxcdv1.FluxReconcilerStatus) int {
 		return cmp.Compare(i.APIVersion+i.Kind, j.APIVersion+j.Kind)
 	})
+
+	return resStats, multiErr
+}
+
+func (r *FluxStatusReporter) getOperatorReconcilersStatus(ctx context.Context) ([]fluxcdv1.FluxReconcilerStatus, error) {
+	var multiErr error
+	crds := []schema.GroupVersionKind{
+		fluxcdv1.GroupVersion.WithKind(fluxcdv1.ResourceSetKind),
+		fluxcdv1.GroupVersion.WithKind(fluxcdv1.ResourceSetInputProviderKind),
+	}
+	resStats := make([]fluxcdv1.FluxReconcilerStatus, len(crds))
+	for i, gvk := range crds {
+		var total int
+		var suspended int
+		var failing int
+
+		list := unstructured.UnstructuredList{
+			Object: map[string]any{
+				"apiVersion": gvk.Group + "/" + gvk.Version,
+				"kind":       gvk.Kind,
+			},
+		}
+
+		if err := r.List(ctx, &list, client.InNamespace("")); err == nil {
+			total = len(list.Items)
+
+			for _, item := range list.Items {
+				if ssautil.AnyInMetadata(&item, map[string]string{fluxcdv1.ReconcileAnnotation: fluxcdv1.DisabledValue}) {
+					suspended++
+				}
+
+				if obj, err := status.GetObjectWithConditions(item.Object); err == nil {
+					for _, cond := range obj.Status.Conditions {
+						if cond.Type == meta.ReadyCondition && cond.Status == corev1.ConditionFalse {
+							failing++
+						}
+					}
+				}
+			}
+		} else {
+			multiErr = kerrors.NewAggregate([]error{multiErr, err})
+		}
+
+		resStats[i] = fluxcdv1.FluxReconcilerStatus{
+			APIVersion: gvk.Group + "/" + gvk.Version,
+			Kind:       gvk.Kind,
+			Stats: fluxcdv1.FluxReconcilerStats{
+				Running:   total - suspended,
+				Failing:   failing,
+				Suspended: suspended,
+			},
+		}
+	}
 
 	return resStats, multiErr
 }
