42182e13720c04281d5af91663fc71d9befe4c51
Matheus Pimenta <matheuscscp@gmail.com>
2025-12-12 23:58:25 +0000

    web: reduce amount of calls to oidc provider
    Signed-off-by: Matheus Pimenta <matheuscscp@gmail.com>

--- internal/web/auth/oidc.go
+++ internal/web/auth/oidc.go
@@ -32,23 +32,29 @@ func newOIDCProvider(conf *config.ConfigSpec) (oauth2Provider, error) {
 	}, nil
 }
 
-// provider creates the oidc.Provider.
-func (o *oidcProvider) provider(ctx context.Context) (*oidc.Provider, error) {
+// init implements oauth2Provider.
+func (o *oidcProvider) init(ctx context.Context) (initializedOAuth2Provider, error) {
 	p, err := oidc.NewProvider(ctx, o.conf.Authentication.OAuth2.IssuerURL)
 	if err != nil {
-		return nil, fmt.Errorf("failed to create OIDC provider: %w", err)
+		return nil, fmt.Errorf("failed to discover OIDC configuration: %w", err)
 	}
-	return p, nil
+	return &initializedOIDCProvider{
+		conf:          o.conf,
+		processClaims: o.processClaims,
+		provider:      p,
+	}, nil
 }
 
-// verifyAccessToken implements oauth2Provider.
-func (o *oidcProvider) verifyAccessToken(ctx context.Context, accessToken string) (string, []string, error) {
-	p, err := o.provider(ctx)
-	if err != nil {
-		return "", nil, err
-	}
+// initializedOIDCProvider implements initializedOAuth2Provider.
+type initializedOIDCProvider struct {
+	conf          *config.ConfigSpec
+	processClaims claimsProcessorFunc
+	provider      *oidc.Provider
+}
 
-	v := p.VerifierContext(ctx, &oidc.Config{ClientID: o.conf.Authentication.OAuth2.ClientID})
+// verifyAccessToken implements initializedOAuth2Provider.
+func (i *initializedOIDCProvider) verifyAccessToken(ctx context.Context, accessToken string) (string, []string, error) {
+	v := i.provider.VerifierContext(ctx, &oidc.Config{ClientID: i.conf.Authentication.OAuth2.ClientID})
 	idToken, err := v.Verify(ctx, accessToken)
 	if err != nil {
 		return "", nil, fmt.Errorf("failed to verify OIDC ID token: %w", err)
@@ -59,7 +65,7 @@ func (o *oidcProvider) verifyAccessToken(ctx context.Context, accessToken string
 		return "", nil, fmt.Errorf("failed to extract claims from OIDC ID token: %w", err)
 	}
 
-	cr, err := o.processClaims(ctx, claims)
+	cr, err := i.processClaims(ctx, claims)
 	if err != nil {
 		return "", nil, fmt.Errorf("failed to process claims from OIDC ID token: %w", err)
 	}
@@ -67,13 +73,13 @@ func (o *oidcProvider) verifyAccessToken(ctx context.Context, accessToken string
 	return cr.username, cr.groups, nil
 }
 
-// verifyToken implements oauth2Provider.
-func (o *oidcProvider) verifyToken(ctx context.Context, token *oauth2.Token) (string, []string, *authStorage, error) {
+// verifyToken implements initializedOAuth2Provider.
+func (i *initializedOIDCProvider) verifyToken(ctx context.Context, token *oauth2.Token) (string, []string, *authStorage, error) {
 	idToken, ok := token.Extra("id_token").(string)
 	if !ok {
 		return "", nil, nil, fmt.Errorf("no id_token found in token response")
 	}
-	username, groups, err := o.verifyAccessToken(ctx, idToken)
+	username, groups, err := i.verifyAccessToken(ctx, idToken)
 	if err != nil {
 		return "", nil, nil, err
 	}
@@ -90,14 +96,10 @@ func (o *oidcProvider) verifyToken(ctx context.Context, token *oauth2.Token) (st
 	return username, groups, as, nil
 }
 
-// config implements oauth2Provider.
-func (o *oidcProvider) config(ctx context.Context) (*oauth2.Config, error) {
-	p, err := o.provider(ctx)
-	if err != nil {
-		return nil, err
-	}
+// config implements initializedOAuth2Provider.
+func (i *initializedOIDCProvider) config() *oauth2.Config {
 	return &oauth2.Config{
-		Endpoint: p.Endpoint(),
+		Endpoint: i.provider.Endpoint(),
 		Scopes:   []string{oidc.ScopeOpenID, oidc.ScopeOfflineAccess, "profile", "email", "groups"},
-	}, nil
+	}
 }
