f06d0fe31f38e00c908272c39975f1dfa5f6b67d
Stefan Prodan <stefan.prodan@gmail.com>
2026-06-09 15:21:46 +0300

    mcp: Remove the hints from `get_kubernetes_resources` tool
    Signed-off-by: Stefan Prodan <stefan.prodan@gmail.com>

--- cmd/mcp/toolbox/get_resource.go
+++ cmd/mcp/toolbox/get_resource.go
@@ -8,8 +8,6 @@ import (
 
 	"github.com/modelcontextprotocol/go-sdk/mcp"
 	"k8s.io/apimachinery/pkg/runtime/schema"
-
-	fluxcdv1 "github.com/controlplaneio-fluxcd/flux-operator/api/v1"
 )
 
 const (
@@ -76,45 +74,5 @@ func (m *Manager) HandleGetKubernetesResources(ctx context.Context, request *mcp
 		return NewToolResultError("No resources found")
 	}
 
-	return NewToolResultText(result + m.GetFluxTip(input.Kind))
-}
-
-func (m *Manager) GetFluxTip(kind string) string {
-	switch kind {
-	case fluxcdv1.FluxGitRepositoryKind, fluxcdv1.FluxBucketKind, fluxcdv1.FluxOCIRepositoryKind:
-		return `
-If asked for recommendations:
-1. Check if the interval is less than 1 minute and if so, recommend to increase it to one minute.
-2. Check if the GitRepository has ref.branch set and if so, recommend to set ref.name to refs/heads/<branch name>.
-3. Check if the GitRepository has ref.tag set and if so, recommend to set ref.name to refs/tags/<tag name>.
-`
-	case fluxcdv1.FluxKustomizationKind:
-		return `
-If asked for recommendations:
-1. Check if the Kustomization interval is less than 10 minutes and if so, recommend to increase it.
-   Explain that the Kustomization interval is for detecting drift in cluster and undo kubectl edits.
-   The interval set in the source (GitRepository, OCIRepository or Bucket) of the Kustomization
-   is for detecting changes in upstream, and that one can be set to a lower value.
-2. Check if the Kustomization has a retryInterval and if not, recommend to add it.
-3. Check if the Kustomization has wait set to true and if so, recommend to set a timeout value.
-4. Check if the Kustomization has prune set to true and if so, recommend to set spec.deletionPolicy to Delete.
-5. Check if the Kustomization has force set to true and if so, recommend to remove it.
-   Explain that force recreates resources and can cause downtime,
-   it should be used only in emergencies when patching fails due to immutable field changes.
-`
-	case fluxcdv1.FluxHelmReleaseKind:
-		return `
-If asked for recommendations:
-1. Check if the interval is less than 10 minutes and if so, recommend to increase it.
-   Explain that the HelmRelease interval is for detecting drift in cluster.
-   The interval set in the source (OCIRepository, HelmRepository) of the HelmRelease
-   is for detecting changes in upstream Helm chart, and that one can be set to a lower value.
-2. Check if the HelmRelease has releaseName set and if not, recommend to add it.
-3. Check if the HelmRelease has targetNamespace set, if so check if storageNamespace is set to the same value.
-   If not, recommend to set storageNamespace to the same value as targetNamespace.
-4. Check if postRenderers are set, if any of the patches have a namespace set in the target, recommend to remove it.
-`
-	default:
-		return ""
-	}
+	return NewToolResultText(result)
 }
