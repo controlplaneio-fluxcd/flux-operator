003f319833d99ce53fa05671cdc5418a639cb94f
Steffo <me@steffo.eu>
2026-09-14 04:29:18 +0200

    Use more significant digits so it doesn't look like it's spinning off-center

--- web/src/components/dashboards/resource/ResourcePage.jsx
+++ web/src/components/dashboards/resource/ResourcePage.jsx
@@ -102,7 +102,7 @@ function getStatusInfo(status) {
       borderColor: 'border-blue-500',
       icon: (
         <svg class="w-10 h-10 text-blue-600 dark:text-blue-400 animate-spin" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m20 4v5h-0.58m-15 2a8 8 0 0 1 15-2m0 0h-4.4m-11 11v-5h0.58m0 0a8 8 0 0 0 15-2m-15 2h4.4" />
+          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m20 4v5h-0.582m-15.356 2a8.001 8.001 0 0 1 15.356-2m0 0h-4.418m-11 11v-5h0.581m0 0a8.003 8.003 0 0 0 15.357-2m-15.357 2h4.419" />
         </svg>
       )
     }
