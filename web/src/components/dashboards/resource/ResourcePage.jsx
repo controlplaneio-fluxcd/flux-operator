23d3c51a731e1791baca545a937c37958b24afab
Stefano Pigozzi <me@steffo.eu>
2026-09-14 04:18:22 +0200

    Flip "Progressing" icon so that it spins in the correct direction
    Signed-off-by: Stefano Pigozzi <me@steffo.eu>

--- web/src/components/dashboards/resource/ResourcePage.jsx
+++ web/src/components/dashboards/resource/ResourcePage.jsx
@@ -102,7 +102,7 @@ function getStatusInfo(status) {
       borderColor: 'border-blue-500',
       icon: (
         <svg class="w-10 h-10 text-blue-600 dark:text-blue-400 animate-spin" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 4v5h.582m15.356 2A8.001 8.001 0 004.582 9m0 0H9m11 11v-5h-.581m0 0a8.003 8.003 0 01-15.357-2m15.357 2H15" />
+          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m20 4v5h-0.58m-15 2a8 8 0 0 1 15-2m0 0h-4.4m-11 11v-5h0.58m0 0a8 8 0 0 0 15-2m-15 2h4.4" />
         </svg>
       )
     }
